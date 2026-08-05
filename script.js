3d6eca3a82fe3dff0570e70ad296dcdbff8d9f01
diamonds1030-droid <diamonds.1030@gmail.com>
2026-08-05 15:27:40 +0900

    script.js を更新

--- script.js
+++ script.js
@@ -2116,6 +2116,7 @@ function initializeButtons() {
     document.getElementById("matchButton")?.addEventListener("click", async () => {
 
         await loadTeams();
+        refreshまtch();
         createLineup();
         createSubstitutionArea();
         showPage("matchPage");
